132a6a55738ba88195b320f5d553badbf1ae7fe5
vengine <vengine@vengines-MacBook-Pro.local>
2020-06-26 15:07:20 +0900

    Initial commit

---
+++ src/main/java/com/youwiz/head1ton/Head1tonApplication.java
@@ -0,0 +1,13 @@
+package com.youwiz.head1ton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Head1tonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Head1tonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/youwiz/head1ton/Head1tonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youwiz.head1ton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Head1tonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
